f8b95746211e937953e489dd1094f3913474874e
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2023-08-15 08:49:54 +0530

    Time: 5 ms (52.47%), Space: 6 MB (10.67%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -4,9 +4,8 @@ public:
        long long int x=0;
        for(int i=1;;i++)
        {
-          // x+=i;
-           n-=i;
-           if(n<0)
+           x+=i;
+           if(x>n)
                return i-1;
        }
         return 0;
