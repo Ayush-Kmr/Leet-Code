afe454e3f43d91f8bae69fc6da98d0fceeb4738e
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2023-08-17 23:30:16 +0530

    Time: 3 ms (84.17%), Space: 13.9 MB (64.22%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n= nums.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low + (high-low)/2;
+            if(nums[mid]==target){
+                cout<<mid<<" ";
+                return true;
+            }
+            if(nums[mid]>=nums[low]){
+                if(nums[mid]==nums[low]){
+                    low++;
+                }
+                else
+                if(target>=nums[low] && target<=nums[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]==nums[high]){
+                    high--;
+                }
+                else
+                if(target>nums[mid] && target<=nums[high]){
+                    low=mid+1;
+                }
+                else{
+                high=mid-1;
+                }
+            }
+        }
+            return false;
+    }
+};
