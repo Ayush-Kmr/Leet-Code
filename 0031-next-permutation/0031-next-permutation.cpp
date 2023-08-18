7254ed66627fc209e809d17a20f292a0bb480193
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2023-08-19 00:47:03 +0530

    Time: 0 ms (100.00%), Space: 12.2 MB (22.03%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -5,17 +5,18 @@ public:
         int value=0;
         for(int i=n-1; i>0; i--)
             if(nums[i]>nums[i-1])
-                 {
+            {
                 value=i-1;
-                 break;
-                 }
+                break;
+            }
         for(int j=n-1; j>0; j--)
             if(nums[value]<nums[j])
-                {
+            {
                 swap(nums[value],nums[j]);
-        reverse(nums.begin()+value+1,nums.end());
-        return;
-                }
-         reverse(nums.begin(),nums.end());
+                reverse(nums.begin()+value+1,nums.end());
+                return;
+            }
+        
+        reverse(nums.begin(),nums.end());
     }
 };
