b2e7ff90d6941b0a50fdf5e89dd8556ddfae1bf4
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2023-10-01 02:05:04 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (20.65%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    string helper(string s)
-    {
-       string ans;
-        
-        for (auto i :s)
-        {
+    string func(string s){
+        string ans;
+        for(auto i:s){
             if(i!='#')
-            {
                 ans.push_back(i);
-            }
-            else if(!ans.empty())
-            {
+            else if(!ans.empty()){
                 ans.pop_back();
             }
         }
         return ans;
     }
     bool backspaceCompare(string s, string t) {
-        
-        return helper(s)== helper(t);
-        
+        return func(s)== func(t);
     }
-};
+}; 
