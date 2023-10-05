76c73c19541424558a3ecf73e222660fd98ad19c
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2023-10-06 02:21:58 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (43.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,15 +3,15 @@ public:
     int climbStairs(int n) {
         int x=1;
         int y=1;
+       int nextstep;
         if(n==1){
             return 1;
         }
-        int nextstep;
         for(int i=1; i<n; i++){
-            nextstep=x+y;
+           nextstep = x+y;
             x=y;
             y=nextstep;
         }
-    return nextstep;
+        return nextstep;
     }
 };
