4816c88f793edea1f2b4b453478def7fa4614a49
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2023-10-07 03:11:00 +0530

    Time: 5 ms (14.98%), Space: 6.4 MB (6.41%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0)return 0;
-        long long x=n;
-      return( x&(x-1))==0;   
+       if(n<=0)return 0;
+        int x=log2(n);
+        return pow(2,x)==n;
     }
 };
