4106619238ca5df28996780c8c29b1d69f362357
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2023-10-08 01:22:53 +0530

    Time: 5 ms (14.06%), Space: 17.4 MB (6.48%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        solve( 0,{}, nums);
+        return ans;
+    }
+    void solve(int p, vector<int>store, vector<int>nums){
+        if(p>=nums.size())
+          {  ans.push_back(store);
+          return;}
+        solve(p+1, store, nums);
+        store.push_back(nums[p]);
+        solve(p+1,store,nums);
+    }
+};
