0bf26bed8cffff161affe466a77bcc16a0c7a006
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2023-10-10 03:00:28 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (28.51%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int fac(int n)
+    { 
+        if (n<=0)return 1;
+        return n* fac(n-1);
+    }
+string solve(int p, int n, int k, string s)
+{
+    if (p >= n )
+    {
+        return s;
+    }
+
+    int f = fac(n - p - 1);
+
+    int q = k / f;
+
+    swap(s[p + q], s[p]);
+    sort(s.begin() + p + 1, s.end());
+    return solve(p + 1, n, k % f, s);
+}
+
+    string getPermutation(int n, int k) {
+        string s="";
+            for(int i=1;i<=n;i++)
+                s+=('0'+i);
+        if(k==1)return s;
+      return  solve(0,n,k-1,s);
+        
+    }
+};
