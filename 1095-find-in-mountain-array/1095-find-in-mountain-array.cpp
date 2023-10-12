fb62ce8baaaefd299742e3ca656ebeb45454302c
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2023-10-13 03:12:52 +0530

    Time: 3 ms (78.73%), Space: 7.3 MB (44.88%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -0,0 +1,44 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
+class Solution {
+public:
+    int search(MountainArray &nums,int t,int l,int h,bool flag)
+    {  while(l<=h)
+        {int m=l+(h-l)/2;
+        int mid=nums.get(m);
+     if(mid==t)return m;
+     bool b=t>mid;
+         if(b^flag)
+             l=m+1;
+         else h=m-1;
+         }
+     return -1;
+    }
+    int findInMountainArray(int t, MountainArray &nums) {
+        int n=nums.length();
+        int l=0;
+        int h=n-1;
+        while(l<h)
+        {
+            int m=l+(h-l)/2;
+            int mid=nums.get(m);
+            if(nums.get(m+1)>mid)
+                l=m+1;
+            else h=m;
+            
+        }
+        
+        int s1=search(nums,t,0,l,0);
+       // cout<<s1<<"k";
+        if(s1!=-1)return s1;
+        return search(nums,t,l,n-1,1);
+    }
+};
