34c162388dd545cf07cb689cd6a3f01c43c9795c
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2023-10-17 02:36:45 +0530

    Time: 16 ms (72.35%), Space: 31.3 MB (59.00%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -7,8 +7,8 @@ public:
         int l=0;
         for(int i=0;i<n;i++)
         {
-            int x=s-l-nums[i];
-            if(x==l)return i;
+           
+            if(s-nums[i]==2*l)return i;
             l+=nums[i];
         }
         return -1;
