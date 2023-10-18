044612e3d0f1a4ca195201b9272daf5d4759e0c4
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2023-10-19 01:49:14 +0530

    Time: 3 ms (48.79%), Space: 6.9 MB (14.93%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        s=" "+s;
         int n=s.size()-1;
         int ans=0;
         for(int i=n;i>=0;i--)
@@ -10,6 +9,6 @@ public:
                 ans++;
             else if(ans>0 )return ans;
         }
-       return 0;
+       return ans;
     }
 };
