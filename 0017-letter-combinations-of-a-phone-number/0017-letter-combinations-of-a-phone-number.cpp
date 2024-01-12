a014abadc338d044a029ab224c093e6938bd4008
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-01-13 01:34:15 +0530

    Time: 0 ms (100.00%), Space: 8 MB (5.20%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -21,7 +21,7 @@ public:
         return ans;
     }
     
-    void helper(int index, string digits, string &temp, vector<string>&ans,         unordered_map<char,string>mp){
+    void helper(int index, string digits, string temp, vector<string>&ans,         unordered_map<char,string>mp){
         int n=digits.size();
         if(index==n){
             ans.push_back(temp);
@@ -29,9 +29,9 @@ public:
         string key = mp[digits[index]];
         int m= key.size();
         for(int i=0; i<m; i++){
-            temp.push_back(key[i]);
-            helper(index+1,digits,temp,ans,mp);
-            temp.pop_back();
+            // temp.push_back(key[i]);
+            helper(index+1,digits,temp+key[i],ans,mp);
+            // temp.pop_back();
         }
     }
 };
