5a58fdc9707652d8ca11f45c2314c31d4505831a
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-01-14 01:00:37 +0530

    Time: 3 ms (66.33%), Space: 6.3 MB (41.13%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long long a= 0;
+        long long b =sqrt(c);
+        while(a<=b){
+            if((a*a)+(b*b) == c){
+                return true;
+            }
+            else if((a*a) + (b*b) < c){
+                a++;
+            }
+            else{
+                b--;
+            }
+        }
+        return false;
+    }
+};
