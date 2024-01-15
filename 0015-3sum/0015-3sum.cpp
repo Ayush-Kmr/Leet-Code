44fe360d4c7ddba231c8f025d129880da6c08663
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-01-15 11:04:19 +0530

    Time: 125 ms (61.06%), Space: 24.6 MB (64.47%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -8,21 +8,21 @@ public:
             int target = -nums[i];
             int start=i+1;
             int end = n-1;
-        if(i!=0 && nums[i]==nums[i-1])continue;
-        while(start<end){
-            if(nums[start]+nums[end]==target){
-                ans.push_back({nums[i],nums[start],nums[end]});
-                start++;
-                end--;
-                while(start<end && nums[start]==nums[start-1])start++;
-                while(start<end && nums[end]==nums[end+1])end--;
-            }
-            else if(nums[start]+nums[end]<=target){
-                start++;
-            }
-            else{
-                end--;
-            }
+            if(i!=0 && nums[i]==nums[i-1]) continue;
+            while(start<end){
+                if(nums[start] + nums[end] == target){
+                    ans.push_back({nums[i],nums[start],nums[end]});
+                    start++;
+                    end--;
+                    while(start<end && nums[start]== nums[start-1]) start++;
+                    while(start<end && nums[end]== nums[end+1]) end--;
+                }
+                else if(nums[start] + nums[end]<target){
+                    start++;
+                }
+                else{
+                    end--;
+                }
         }
             
     }
