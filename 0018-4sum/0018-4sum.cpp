151fb8aac9ad32c150a537eb821b0054f2b216e9
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-01-16 23:22:19 +0530

    Time: 92 ms (36.39%), Space: 13.5 MB (97.02%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        set<vector<int>>ans1;
+       vector<vector<int>>ans;
         int n = nums.size();
         sort(nums.begin(),nums.end());
         for(int i=0; i<n; i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
             for(int j=i+1; j<n; j++){
                 long long value = target - nums[i];
                 int start = j+1;
                 int end = n-1;
+                 if(j>i+1 && nums[j]==nums[j-1])continue;
                 while(start<end){
-                    if(nums[start] + nums[end]  == value- nums[j]){
-                        ans1.insert({nums[i],nums[j],nums[start],nums[end]});
+                    if(nums[start] + nums[end]  == value - nums[j]){
+                        ans.push_back({nums[i],nums[j],nums[start],nums[end]});
                         start++;
                         end--;
+                        while(start<end && nums[start]==nums[start-1]) start++;
+                        while(start<end && nums[end]==nums[end+1]) end--;
                     }
-                    else if(nums[start] + nums[end] < value- nums[j]){
+                    else if(nums[start] + nums[end] < value - nums[j]){
                         start++;
                     }
                     else{
@@ -24,7 +28,7 @@ public:
                 }
             }
         }
-        vector<vector<int>>ans(ans1.begin(),ans1.end());
+        
         return ans;
     }
 };
