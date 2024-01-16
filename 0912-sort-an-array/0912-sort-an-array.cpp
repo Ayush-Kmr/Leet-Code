103f662fe169f062150fce994fe375e9c2a6823f
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-01-17 01:52:19 +0530

    Time: 427 ms (34.46%), Space: 145.3 MB (31.00%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,7 +1,49 @@
 class Solution {
 public:
+    
+    void merge(vector<int>& nums, int start, int mid, int end) {
+        int idx1 = start, idx2 = mid + 1;
+        int i = 0;
+        vector<int> temp(end - start + 1);
+
+        while (idx1 <= mid && idx2 <= end) {
+            if (nums[idx1] < nums[idx2]) {
+                temp[i++] = nums[idx1++];
+            } else {
+                temp[i++] = nums[idx2++];
+            }
+        }
+
+        while (idx1 <= mid) {
+            temp[i++] = nums[idx1++];
+        }
+
+        while (idx2 <= end) {
+            temp[i++] = nums[idx2++];
+        }
+
+        for (int k = 0; k < i; k++) {
+            nums[start + k] = temp[k];
+        }
+    }
+
+//     =========================================================================
+    void mergesort(vector<int>&nums, int start, int end){
+        if(start < end){
+            int mid = start + (end-start)/2;
+            mergesort(nums,start,mid);
+            mergesort(nums,mid+1,end);
+            merge(nums,start,mid,end);
+        }
+    }
+    
+//     ===========================================================================
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        int start=0;
+        int end = n-1;
+        mergesort(nums,start,end);
+        
         return nums;
     }
 };
