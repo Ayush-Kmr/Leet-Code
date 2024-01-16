5f1f4dffd5c5050c6d5472606234cf1553e0c914
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-01-17 00:48:01 +0530

    Time: 40 ms (33.11%), Space: 10.5 MB (54.64%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        long long ans = nums[0] + nums[1] + nums[2];
-        for (int i = 0; i < n; i++) {
-            int j = i + 1, k = n - 1;
-            while (j < k) {
-            long long curr_sum = static_cast<long long>(nums[i]) + nums[j] + nums[k];
-                if (abs(ans - target) > abs(curr_sum - target)) {
-                    ans = curr_sum;
-                }
-                if (curr_sum > target) {
-                    k--;
-                } else {
-                    j++;
-                }
+        sort(nums.begin(),nums.end());
+        int  n  = nums.size();
+        long long sum = nums[0] + nums[1] + nums[2];
+        
+        for(int i=0; i<n; i++){
+            int start =i+1;
+            int end= n-1;
+            while(start<end){
+    long long currsum =  static_cast<long long>(nums[i]+nums[start]+nums[end]);
+            if(abs(sum-target)>=abs(currsum-target)) sum =currsum;
+             if(currsum>target) end--;
+             else start++;
             }
         }
-        return static_cast<int>(ans);
+        return static_cast<int>(sum);
     }
 };
