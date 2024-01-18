92d6d784b47f809c2b646b7f01e7e7b0bd44b6ac
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-01-18 11:59:13 +0530

    Time: 6 ms (96.97%), Space: 8.7 MB (65.88%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-bool isValid(string s) {
-        stack<char> temp;
-
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == 'c') {
-                if (temp.size() < 2) return false;
-                
-                char top1 = temp.top();
+    bool isValid(string s) {
+        stack<char>temp;
+        for(int i=0; i< s.size() ; i++){
+            if(s[i]=='c'){
+                if(temp.size()<2) return false;
+            char top1= temp.top();
                 temp.pop();
-                char top2 = temp.top();
+            char top2 = temp.top();
                 temp.pop();
-                
-                if (top2 == 'a' && top1 == 'b') continue;
-                else return false;
-            } else {
+            if(top1=='b' && top2=='a') continue;
+            else return false;
+            }
+            else{
                 temp.push(s[i]);
             }
         }
-
         return temp.empty();
     }
 };
