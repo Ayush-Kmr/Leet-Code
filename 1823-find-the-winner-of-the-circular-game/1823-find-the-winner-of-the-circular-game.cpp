96aa7847f8b9055b884c6362c4b913635ff96325
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-01-20 02:54:00 +0530

    Time: 2 ms (75.21%), Space: 7.2 MB (27.09%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -4,9 +4,11 @@ public:
         if(n==1){
             return 0;
         }
+//         Current answer is depedent on the previous answer with a difference of k
         return (solve(n-1,k)+k)%n;
     }
     int findTheWinner(int n, int k) {
+//         Adding 1 to make 1 based indexing as we used mod operator
         int ans = solve(n,k)+1;
         return ans;
     }
