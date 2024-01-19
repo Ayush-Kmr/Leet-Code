ec1fcd6bd2af942cefaf3e3ac9e6fcde22c09392
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-01-20 01:34:55 +0530

    Time: 7 ms (24.71%), Space: 8.4 MB (5.15%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.cpp
+++ 0390-elimination-game/0390-elimination-game.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
-    int solve(int n){
+    int lastRemaining(int n) {
         if(n==1){
             return 1;
         }
-        
-        return 2*(1+n/2 - solve(n/2));
-    }
-    
-    int lastRemaining(int n) {
-        int x = solve(n);
-        
-        return x;
-         
+        return 2*(1+n/2 - lastRemaining(n/2));  
     }
 };
