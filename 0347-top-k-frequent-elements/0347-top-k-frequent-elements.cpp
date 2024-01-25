f4e47643c2284f630e0417ebf2ea4dc04586dcdc
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-01-26 03:23:25 +0530

    Time: 20 ms (16.46%), Space: 16.7 MB (9.36%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
-    
-    static bool sortcom(pair<int,int>a,pair<int,int>b){
-        return a.second>b.second;
-    }
+
     vector<int> topKFrequent(vector<int>& nums, int k) {
         vector<int>ans;
         map<int,int,greater<int>>mp;
@@ -12,12 +9,8 @@ public:
         for(int i=0; i<n; i++){
             mp[nums[i]]++;
         }
-        // sort(mp.begin(),mp.end(),sortcom);
-        
         for(auto i:mp){
-           
             temp.push_back({i.second,i.first});
-            // k--;
         }
          sort(temp.begin(),temp.end());
         for(int  i=temp.size()-1; k>0 && i>=0; i--,k--){
