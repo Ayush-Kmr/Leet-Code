6bb624f7742c47990287679e3d0131258b1270e0
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-01-26 03:22:51 +0530

    Time: 16 ms (36.30%), Space: 16.5 MB (9.46%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
+    
+    static bool sortcom(pair<int,int>a,pair<int,int>b){
+        return a.second>b.second;
+    }
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int>umap;
-        for(auto x: nums){
-            umap[x]++;
-        }
-        priority_queue<pair <int,int>,vector<pair<int,int>>,greater<pair<int,int> >>pq;
-        for(auto x:umap){
-            pq.push(make_pair(x.second,x.first));
-            if(pq.size()>k) pq.pop();
-        }
         vector<int>ans;
-        while(!pq.empty()){
-            pair<int,int> temp = pq.top();
-            pq.pop();
+        map<int,int,greater<int>>mp;
+        vector<vector<int>>temp;
+        int n= nums.size();
+        for(int i=0; i<n; i++){
+            mp[nums[i]]++;
+        }
+        // sort(mp.begin(),mp.end(),sortcom);
+        
+        for(auto i:mp){
+           
+            temp.push_back({i.second,i.first});
+            // k--;
+        }
+         sort(temp.begin(),temp.end());
+        for(int  i=temp.size()-1; k>0 && i>=0; i--,k--){
+            ans.push_back(temp[i][1]);
             
-            ans.push_back(temp.second);
         }
         return ans;
     }
