dcbf01078433771b9830cf6fb04d70813db4d626
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-01-27 00:19:06 +0530

    Time: 11 ms (93.40%), Space: 21.8 MB (16.16%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    bool check(vector<vector<char>>& board,int row, int col){
-        char x=board[row][col];
+    
+    bool check(vector<vector<char>>& board, int row, int col){
+        char x= board[row][col];
         
+//         Checking the whole row value
         for(int i=0; i<9; i++){
-          if(board[row][i]==x && i!=col) return true;  
-          
+            if(board[row][i]==x && i!=col) return true;
         }
-         for(int j=0; j<9; j++){
-            if(board[j][col]==x && j!=row) return true;     
+        
+//         Checking the whole col value
+        for(int j=0; j<9; j++){
+            if(board[j][col]==x && j!=row) return true;
         }
-        int i=row-row%3;
-        int j=(col/3)*3;
-         for(int l=i; l<i+3; l++){
-            for(int k=j; k<j+3; k++){
-                if(board[l][k]==x && l!=row) return true;
+        
+        int i= row-(row%3);
+        int j= (col/3)*3;
+        
+//         Checking the 3 x 3 boxes
+        for(int r=i; r<i+3; r++){
+            for(int c=j; c<j+3; c++){
+                if(board[r][c]==x && r!=row) return true;
             }
         }
         return false;
-        
     }
     bool isValidSudoku(vector<vector<char>>& board) {
-      
         for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
                 if(board[i][j]!='.' && check(board,i,j)) return false;
