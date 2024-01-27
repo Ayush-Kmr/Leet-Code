6bfe0519d9369f23eadc025ec22f96a7a82b1cae
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-01-27 12:37:08 +0530

    Time: 14 ms (86.42%), Space: 27.7 MB (98.74%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -15,6 +15,7 @@ public:
         if(node1==NULL && node2 == NULL) return true;
         if(!node1 || !node2 ) return false;
         if(node1->val != node2->val) return false;
+        
         return check(node1->left,node2->left)  && check(node1->right,node2->right);
         
         
