2a5a2d0e3967ae35237c3ef2359d3fe23dfdecc0
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-02-02 03:08:32 +0530

    Time: 19 ms (99.21%), Space: 30.1 MB (23.14%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -2,12 +2,37 @@ class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
         int n= nums.size();
-        int ans =0;
-        sort(nums.begin(),nums.end());
+        int f_max = INT_MIN;
+        int s_max = INT_MIN;
+        int t_max = INT_MIN;
+        int f_min = INT_MAX;
+        int s_min = INT_MAX;
+        for(int i =0; i<n; i++){
+            
+//             Three maximum value of the array
+            if(nums[i]>=f_max){
+                t_max = s_max;
+                s_max = f_max;
+                f_max = nums[i];
+            }
+            else if(nums[i] >=s_max && nums[i] != f_max){
+                t_max = s_max;
+                s_max = nums[i];
+            }
+            else if(nums[i]>=t_max && nums[i]!= s_max && nums[i]!=f_max) {
+                t_max = nums[i];
+            }
+            
+//             Two minimum value of the given array
+            if(nums[i]<=f_min){
+                s_min=f_min;
+                f_min = nums[i];
+            }
+            else if(nums[i]<=s_min && f_min!= nums[i]){
+                s_min = nums[i];
+            }
+        }
         
-        ans=nums[n-1]*nums[n-2]*nums[n-3];
-        ans=max(nums[0]*nums[1]*nums[n-1],ans);
-        
-        return max(nums[0]*nums[1]*nums[2],ans);
+       return max( f_min*s_min*f_max, f_max*s_max*t_max);
     }
 };
