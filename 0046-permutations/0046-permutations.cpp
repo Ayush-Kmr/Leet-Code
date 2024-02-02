a22e6ba4b4fe2e715607dee8330f1911ec76e40f
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-02-03 02:14:24 +0530

    Time: 4 ms (44.01%), Space: 8.9 MB (25.57%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     
-       void solve(int k, vector<int>&nums, vector<vector<int>>&ans){
-        if(k>=nums.size()){
+       void solve(int index, vector<int>&nums, vector<vector<int>>&ans){
+//            Base case
+        if(index>=nums.size()){
             ans.push_back(nums);
             return;}
-        for(int i=k; i<nums.size(); i++){
-            swap(nums[k],nums[i]);
-            solve(k+1, nums,ans);
-            swap(nums[k], nums[i]);
+        for(int j=index; j<nums.size(); j++){
+            swap(nums[index],nums[j]);
+            solve(index+1, nums,ans);
+//             Backtrack
+            swap(nums[index], nums[j]);
         }
     }
     
