900a8e51a912ae218f6314f701ef72dc844ddc8a
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-02-04 02:46:08 +0530

    Time: 2 ms (68.69%), Space: 11.7 MB (85.49%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    void solve(string temp, int n, int l, int r,vector<string>&ans){
+    void solve(string &temp, int n, int l, int r,vector<string>&ans){
         if(temp.size()==2*n){
             ans.push_back(temp);
             return;
         }
         if(l<n){
-            solve(temp+'(',n,l+1,r,ans);
+            temp.push_back('(');
+            solve(temp,n,l+1,r,ans);
+            temp.pop_back();
         }
         if(r<l){
-            solve(temp+')',n,l,r+1,ans);
+            temp.push_back(')');
+            solve(temp,n,l,r+1,ans);
+            temp.pop_back();
         }
     }
     vector<string> generateParenthesis(int n) {
         vector<string>ans;
-        solve("",n,0,0,ans);
+        string s ="";
+        solve(s,n,0,0,ans);
         
         return ans;
     }
