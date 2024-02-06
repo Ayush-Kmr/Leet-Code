5c89b152d51ab21302660b638448e5cc30199b73
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-02-07 01:43:30 +0530

    Time: 8 ms (78.33%), Space: 19.9 MB (32.16%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& as) {
+        vector<int>v;
+        int n = as.size();
+        for(int i =0; i<n; i++){
+            if(v.size()==0) v.push_back(as[i]);
+            else{
+                if(as[i]<0){
+                    if(v.back()>0){
+                        if(abs(as[i])>v.back()){
+                            v.pop_back();
+                            // v.push_back(as[i]);
+                            i--;
+                            
+                        }
+                        else if(abs(as[i])==v.back()){
+                            v.pop_back();
+                        }
+                    }
+                    else{
+                        v.push_back(as[i]);
+                    }
+                }
+                else{
+                    v.push_back(as[i]);
+                }
+            }
+        }
+        // reverse(v.begin(),v.end());
+        return v;
+    }
+};
