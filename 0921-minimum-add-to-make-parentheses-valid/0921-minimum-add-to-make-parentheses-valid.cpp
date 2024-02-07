51ddfe577f8243b5555f09b45a57d3905f589634
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-02-07 10:39:01 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (13.35%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -8,17 +8,13 @@ public:
                  st.push(i);
                  count++;
             }
-            // else if(i==')' && st.empty()) count++;
             else{
                 if(i==')' && !st.empty() && st.top()=='('){
                     count--;
                     st.pop();
                 }
-                else if(i==')' && !st.empty() && st.top()!='('){
-                        count++;
-                }
                 else {
-                    count++;
+                        count++;
                 }
             }   
         }
