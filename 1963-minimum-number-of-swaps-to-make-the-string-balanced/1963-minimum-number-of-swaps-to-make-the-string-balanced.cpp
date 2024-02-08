ea55c6d9931567de67ff97132c6ca81b6ed89532
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-02-09 00:50:07 +0530

    Time: 108 ms (50.23%), Space: 34.8 MB (19.69%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -10,5 +10,19 @@ public:
         }
         if(!st.empty()) count++;
         return count/2;
+        
+        
+//      Another logic   
+        // int open=0;
+        // int count =0;
+        // for(auto i:s){
+        //     if(i=='[') open++;
+        //     else open--;
+        //     count = min(count,open);
+        // }
+        // return (abs(count)+1)/2;
     }
 };
+
+
+
