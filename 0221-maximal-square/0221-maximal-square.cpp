5a6e685a0e20f5ebb8bbc457158ce264456354a5
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-02-10 15:44:35 +0530

    Time: 71 ms (48.72%), Space: 24.4 MB (26.10%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-// //     Top Down Approacch
-//     int solve(vector<vector<char>>& matrix, int i, int j, int &maxi,vector<vector<int>>&dp){
-// //         base case
-//         if(i>=matrix.size() || j>= matrix[0].size()){
-//             return 0;
-//         }
-//         if(dp[i][j]!=-1){
-//             return dp[i][j];
-//         }
-//         int bottom = solve(matrix, i+1,j,maxi,dp);
-//         int diagonal = solve(matrix, i+1,j+1,maxi,dp);
-//         int right = solve(matrix, i , j+1,maxi,dp);
+//     Top Down Approacch
+    int solve(vector<vector<char>>& matrix, int i, int j, int &maxi,vector<vector<int>>&dp){
+//         base case
+        if(i>=matrix.size() || j>= matrix[0].size()){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int bottom = solve(matrix, i+1,j,maxi,dp);
+        int diagonal = solve(matrix, i+1,j+1,maxi,dp);
+        int right = solve(matrix, i , j+1,maxi,dp);
         
-//         if(matrix[i][j] == '1'){
-//             int side = min(bottom, min(diagonal,right)) + 1; 
-//             maxi = max(maxi,side);
-//             return dp[i][j] = side;
-//         }
-//         else return 0;  
+        if(matrix[i][j] == '1'){
+            int side = min(bottom, min(diagonal,right)) + 1; 
+            maxi = max(maxi,side);
+            return dp[i][j] = side;
+        }
+        else return 0;  
         
-//     }
+    }
     
     
 //     Bottom Up approach
