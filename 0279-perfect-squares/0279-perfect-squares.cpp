066d6fa7889882b053216e57ef96eebffdef57a7
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-02-10 11:24:57 +0530

    Time: 48 ms (62.65%), Space: 10.5 MB (45.88%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
-    int solve(int n,vector<int>&dp){
-//         Base Case
-        if(n==0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        int ans = INT_MAX;
+    
+// //   Top Down
+//     int solve(int n,vector<int>&dp){
+// //         Base Case
+//         if(n==0) return 0;
+//         if(dp[n]!=-1) return dp[n];
+//         int ans = INT_MAX;
+//         for(int i=1; i*i<=n; i++){
+//             ans = min(ans,1+solve(n-i*i,dp));
+//         }
+//         return dp[n] = ans;
+//     }
+    
+//     Bottom UP
+    int solveTab(int N){
+    vector<int>dp(N+1,0);
+        for(int n=1; n<=N; n++){
+            int ans = INT_MAX;
         for(int i=1; i*i<=n; i++){
-            ans = min(ans,1+solve(n-i*i,dp));
+            ans = min(ans,1+dp[n-i*i]);
         }
-        return dp[n] = ans;
+         dp[n] = ans;
+        }
+        return dp[N];
     }
     int numSquares(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        // vector<int>dp(n+1,-1);
+        // return solve(n,dp);
+        return solveTab(n);
     }
 };
