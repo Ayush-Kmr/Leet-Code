82cac9b9371fdce9ddf36cb91ee1d7523d846714
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-02-12 00:40:01 +0530

    Time: 2049 ms (11.04%), Space: 7.2 MB (18.10%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+//     Catalan Number main logic is here
+    int solve(int n){
+        if(n<=1) return 1;
+        int ans =0;
+        for(int i=1; i<=n; i++){
+            ans += solve(i-1) * solve(n-i);
+        }
+        return ans;
+    }
+    int numTrees(int n) {
+        return solve(n);
+        
+    }
+};
