82cf593a8aab5dc963aa1168192a844715bf20db
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-02-11 17:46:03 +0530

    Time: 85 ms (55.57%), Space: 74.2 MB (26.62%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+//     Top Down Approach
     int solve(string &s, string &rev, int i, int j,vector<vector<int>>&dp){
         if(i==s.length() || j==rev.length()) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
@@ -14,11 +14,34 @@ public:
         }
         return dp[i][j];
     }
+    
+    
+//     Bottom Up Approach
+    int solveTab (string &s, string &rev){
+        vector<vector<int>>dp(s.length()+1, vector<int>(rev.length()+1,0));
+        for(int i=s.length()-1; i>=0; i--){
+            for(int j =rev.length()-1; j>=0; j--){
+            int res =0;
+            if(s[i]==rev[j]){
+            res = 1 + dp[i+1][j+1];
+        }
+        else{
+        int op1 = dp[i+1][j];
+        int op2 = dp[i][j+1];
+        res = max(op1,op2);
+        }
+        dp[i][j] = res;
+            }
+        }
+        return dp[0][0];
+    }
     int longestPalindromeSubseq(string s) {
         string rev = s;
         reverse(rev.begin(),rev.end());
-        vector<vector<int>>dp(s.length()+1, vector<int>(rev.length()+1,-1));
-        int ans = solve(s,rev,0,0,dp);
+        // vector<vector<int>>dp(s.length()+1, vector<int>(rev.length()+1,-1));
+        // int ans = solve(s,rev,0,0,dp);
+        
+        int ans = solveTab(s,rev);
         return ans;
     }
 };
