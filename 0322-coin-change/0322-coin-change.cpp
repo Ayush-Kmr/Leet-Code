1772385429d08ca21227fc57ed97fe86311ffb63
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-02-15 01:30:29 +0530

    Time: 23 ms (88.39%), Space: 15.9 MB (60.95%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//     Top Down
 //     int solve(vector<int>& coins,int amount, vector<int>&dp){
 //         if(amount == 0){
 //             return 0;
@@ -19,19 +20,14 @@ public:
     int solve(vector<int>& coins, int amount){
         vector<int>dp(amount+1, amount+1);
         dp[0]=0;
-        
         for(int count =0; count<=amount; count++){
-           
            for(int i=0; i<coins.size(); i++){
                if(count-coins[i]>=0)
                dp[count] = min(dp[count],1+ dp[count-coins[i]]);
          }
-        
-       
-                     
-        }
-                         return dp[amount];
     }
+          return dp[amount];
+}
     int coinChange(vector<int>& coins, int amount) {
         // vector<int>dp(amount+1, -1);
         // int ans=solve(coins,amount,dp);
