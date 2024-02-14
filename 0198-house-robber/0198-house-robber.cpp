bf7552cc0f0fc1b297744e7b1d09feba6807228e
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-02-14 23:06:12 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (39.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//     Top Down Dp
     int solve(vector<int>& nums, int i, int n, vector<int>&dp){
         if(i>=n){
             return 0;
@@ -10,10 +11,26 @@ public:
         
         return dp[i] = max(include, exclude);
     }
+    
+//     Bottom Up DP
+    int solve(vector<int>& nums, int N)
+    {
+        vector<int>dp(N+2, 0);
+        for(int n = N-1; n>=0; n--){
+        int include = nums[n] + dp[n+2];
+        int exclude = 0 + dp[n+1];
+        
+        dp[n] = max(include, exclude);
+            }
+        return dp[0];   
+    }    
+    
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1, -1);
-        int ans = solve(nums, 0, n,dp);
+        // vector<int>dp(n+1, -1);
+        // int ans = solve(nums, 0, n,dp);
+        
+        int ans = solve(nums,n);
         return ans;
     }
 };
