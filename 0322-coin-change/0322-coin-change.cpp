68a7311be1483074fe32788fa211958ad32c508e
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-02-15 01:29:35 +0530

    Time: 26 ms (84.50%), Space: 15.9 MB (60.95%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
-    int solve(vector<int>& coins,int amount, vector<int>&dp){
-        if(amount == 0){
-            return 0;
-        }
+//     int solve(vector<int>& coins,int amount, vector<int>&dp){
+//         if(amount == 0){
+//             return 0;
+//         }
         
-        if(amount< 0) return 1e9;
-        if(dp[amount]!=-1) return dp[amount]; 
-        int ans = 1e9;
+//         if(amount< 0) return 1e9;
+//         if(dp[amount]!=-1) return dp[amount]; 
+//         int ans = 1e9;
+//            for(int i=0; i<coins.size(); i++){
+//                ans = min(ans,1+solve(coins, amount - coins[i],dp));
+//          }
+        
+//       return dp[amount] = ans;    
+//     }
+    
+//     Bottom Up
+    int solve(vector<int>& coins, int amount){
+        vector<int>dp(amount+1, amount+1);
+        dp[0]=0;
+        
+        for(int count =0; count<=amount; count++){
+           
            for(int i=0; i<coins.size(); i++){
-               ans = min(ans,1+solve(coins, amount - coins[i],dp));
+               if(count-coins[i]>=0)
+               dp[count] = min(dp[count],1+ dp[count-coins[i]]);
          }
         
-      return dp[amount] = ans;    
+       
+                     
+        }
+                         return dp[amount];
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1, -1);
-        int ans=solve(coins,amount,dp);
-       return ans==1e9?-1:ans  ;
+        // vector<int>dp(amount+1, -1);
+        // int ans=solve(coins,amount,dp);
+        int ans = solve(coins , amount);
+       return ans==amount+1?-1:ans  ;
     }
 };
