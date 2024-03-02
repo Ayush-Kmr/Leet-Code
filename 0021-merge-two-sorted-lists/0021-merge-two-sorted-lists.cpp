e777936446074aab018ea45c6cc27218926de47e
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-03-03 02:32:39 +0530

    Time: 7 ms (38.81%), Space: 18.4 MB (29.64%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,16 +11,38 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(!list1) return list2;
-        if(!list2) return list1;
+//         Recursive Approach
+//         if(!list1) return list2;
+//         if(!list2) return list1;
         
-        if(list1->val <= list2->val){
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
+//         if(list1->val <= list2->val){
+//             list1->next = mergeTwoLists(list1->next, list2);
+//             return list1;
+//         }
+//         else{
+//             list2->next = mergeTwoLists(list1, list2->next);
+//             return list2;
+//         }
+        
+//         Iterative Approach
+        ListNode* curr = new ListNode(0);
+        ListNode* ans = curr;
+        while(list1 && list2){
+            if(list1->val <= list2->val){
+                curr->next = list1;
+                list1 = list1->next;
+                curr = curr->next;
+            }
+            else{
+                curr->next = list2;
+                list2 = list2->next;
+                curr = curr->next;
+            }
         }
-        else{
-            list2->next = mergeTwoLists(list1, list2->next);
-            return list2;
+        if(list1 || list2){
+            curr->next = (list1==NULL) ? list2 : list1;
         }
+        
+        return ans->next;
     }
 };
