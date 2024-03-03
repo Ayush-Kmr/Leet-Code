2cb69e09cd8ccffb24463c47ee483bd9e419cfea
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-03-03 06:29:40 +0530

    Time: 300 ms (5.03%), Space: 144.6 MB (5.24%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,46 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* middle(ListNode* head) {
+    ListNode* middle(ListNode* head){
         ListNode* slow = head;
-        ListNode* fast = head;
-        while (fast->next && fast->next->next) {
+        ListNode* fast = head->next;
+        while(fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
         }
         return slow;
     }
-
-    ListNode* mergeSortedList(ListNode* p, ListNode* q) {
-        if (!p || !q) {
-            return (p == NULL) ? q : p;
+    
+    ListNode* mergeSortedList(ListNode* p, ListNode* q){
+        if(!p || !q){
+            return (p==NULL) ? q : p;
         }
         ListNode* curr = new ListNode(0);
         ListNode* ans = curr;
-        while (p && q) {
-            if (p->val < q->val) {
-                curr->next = p;
+        while(p && q){
+            if(p->val <= q->val){
+                curr->next = new ListNode(p->val);
+                curr = curr->next;
                 p = p->next;
-            } else {
-                curr->next = q;
+            }
+            else{
+                curr->next = new ListNode(q->val);
+                curr = curr->next;
                 q = q->next;
             }
-            curr = curr->next;
         }
-        if (p || q) {
-            curr->next = (p != NULL) ? p : q;
+        if(p || q){
+            curr->next = (p==NULL) ? q : p;
         }
         return ans->next;
     }
-
     ListNode* sortList(ListNode* head) {
-        if (!head || !head->next) return head;
+        if(!head || !head->next) return head;
         ListNode* mid = middle(head);
-        ListNode* newhead = mid->next;
+        ListNode* head2 = mid->next;
         mid->next = NULL;
-
+        
         ListNode* left_half = sortList(head);
-        ListNode* right_half = sortList(newhead);
-
+        ListNode* right_half = sortList(head2);
+        
         return mergeSortedList(left_half, right_half);
     }
 };
