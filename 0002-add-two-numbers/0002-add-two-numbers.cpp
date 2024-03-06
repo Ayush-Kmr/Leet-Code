a37887e36e9ed37d40447fc5f98a81ea16a42eae
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-03-06 20:40:34 +0530

    Time: 19 ms (72.97%), Space: 76 MB (39.31%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -17,16 +17,15 @@ public:
         while(l1 || l2){
             int x = (l1!=NULL) ? l1->val : 0;
             int y = (l2!=NULL) ? l2->val : 0;
-            int sum = x+y+carry;
+            int sum = x + y + carry;
             carry = sum/10;
-            curr->next = new ListNode(sum%10);
-            curr = curr->next;
-            
+            curr->next  = new ListNode(sum%10);
             if(l1) l1 = l1->next;
             if(l2) l2 = l2->next;
-        }
+            
+            curr = curr->next;
+            }
         if(carry>0) curr->next = new ListNode(carry);
-        
         return ans->next;
     }
 };
