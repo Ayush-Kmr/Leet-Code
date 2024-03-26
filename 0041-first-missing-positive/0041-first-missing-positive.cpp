1807ec2ba99aebbbcd130ddf597419b1a7a13df5
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-03-26 13:41:03 +0530

    Time: 42 ms (67.55%), Space: 43.6 MB (81.44%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -7,12 +7,11 @@ public:
             if(nums[i]<=0) swap(nums[i],nums[size--]);
         }
         for(int i=0; i<=size; i++){
-            int index=abs(nums[i]);
-            index=index-1;
+            int index = abs(nums[i])-1;
             if(index<=size && nums[index]>0) nums[index]*=-1;
         }
         for(int i=0; i<=size; i++){
-            if(nums[i]>0) return i+1;
+            if(nums[i]>=0) return i+1;
         }
         return size+2;
     }
