1a96c3b26f1e400c0bf12011c3ecd26ce6210433
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-04-01 13:55:23 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (57.50%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int n=s.size()-1;
-        int ans=0;
-        for(int i=n;i>=0;i--)
-        {
-            if(s[i]!=' ')
-                ans++;
-            else if(ans>0 )return ans;
+        bool foundNonSpace = 0;
+        int n = s.length();
+        int count=0;
+    for (int i = n - 1; i >= 0; i--) {
+        if (s[i] != ' ') {
+            count++;
+            foundNonSpace = 1; 
+        } else {
+            if (foundNonSpace) {
+                return count;
+            }
         }
-       return ans;
+    }
+
+    return count; 
     }
 };
