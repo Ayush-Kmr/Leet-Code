5cba249bbb0df1658dd64e1e643e14783e798e67
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-04-03 13:56:24 +0530

    Time: 11 ms (90.03%), Space: 21.7 MB (93.07%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    
-    bool check(vector<vector<char>>& board, int row, int col){
-        char x= board[row][col];
+    bool check(vector<vector<char>>&board, int row , int col){
+        char x = board[row][col];
         
 //         Checking the whole row value
+        for(int i=0; i<9; i++){
+            if(board[i][col]==x && i!=row) return true;
+        }
+//         Checking the whole column value
         for(int j=0; j<9; j++){
             if(board[row][j]==x && j!=col) return true;
         }
         
-//         Checking the whole col value
-        for(int i=0; i<9; i++){
-            if(board[i][col]==x && i!=row) return true;
-        }
-        
-        int i= row-(row%3);
-        int j= (col/3)*3;
-        
-//         Checking the 3 x 3 boxes
+//         Checking 3*3 block 
+        int i = row - (row%3);
+        int j = (col/3)*3;
         for(int r=i; r<i+3; r++){
             for(int c=j; c<j+3; c++){
                 if(board[r][c]==x && r!=row) return true;
