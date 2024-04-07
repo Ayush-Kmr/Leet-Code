544fe0d2a55f8cbe7fdfa17bb01f1ffc4fa4fadd
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-04-07 21:35:29 +0530

    Time: 3 ms (52.26%), Space: 12.8 MB (98.68%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,35 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-         vector<vector<int>>ans;
-        int level =1;
-        if(root==NULL) return ans;
-        queue<TreeNode*>q;
+        vector<vector<int>> ans;
+        
+        if (!root) return ans;
+        
+        queue<TreeNode*> q;
         q.push(root);
-        while(!q.empty()){
+        bool reverseLevel = false; // Flag to track if we need to reverse the level
+        
+        while (!q.empty()) {
             int size = q.size();
-            vector<int>temp;
-            while(size--){
-            TreeNode* curr = q.front();
-            q.pop();
-            temp.push_back(curr->val);
-            if(curr->left) q.push(curr->left);
-            if(curr->right) q.push(curr->right);
+            vector<int> zigzag(size);
+            
+            for (int i = 0; i < size; i++) {
+                TreeNode* temp = q.front();
+                q.pop();
+                
+                int index = reverseLevel ? size - 1 - i : i; // Adjust index based on zigzag pattern
+                
+                zigzag[index] = temp->val;
+                
+                if (temp->left) q.push(temp->left);
+                if (temp->right) q.push(temp->right);
             }
-            if(level%2== 0)reverse(temp.begin(),temp.end());
-            ans.push_back(temp);
-            level++;
+            
+            ans.push_back(zigzag);
+            reverseLevel = !reverseLevel; // Toggle the reverse flag for the next level
         }
         
         return ans;
