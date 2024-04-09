32b5e9aeebacbde83cf96852624ecb2dfb06e216
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-04-09 20:58:18 +0530

    Time: 11 ms (73.15%), Space: 19.3 MB (11.50%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,15 +9,30 @@
  */
 class Solution {
 public:
+    bool isfindPath(TreeNode* root, TreeNode* value, vector<TreeNode*>&path){
+        if(!root) return false;
+        path.push_back(root);
+        if(root == value) return true;
+        if(isfindPath(root->left,value, path) || isfindPath(root->right, value, path)) {
+            return true;
+        }
+        path.pop_back();
+        return false;
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root == NULL) return NULL;
-        if(root == p || root == q) return root;
-        TreeNode* left = lowestCommonAncestor(root->left,p,q);
-        TreeNode* right = lowestCommonAncestor(root->right,p,q);
-        
-        if(left && right) return root;
-        if(left) return left;
-        else if(right) return right;
-        else return NULL;
+        if(!root) return NULL;
+        vector<TreeNode*>path1,path2;
+        if(!isfindPath(root,p,path1) || !isfindPath(root,q,path2)){
+            return NULL;
+        }
+           TreeNode* ans = NULL;
+           int i=0;
+           while(i<path1.size() && i<path2.size()){
+               if(path1[i] == path2[i]){
+                   ans = path1[i];
+               }
+               i++;
+           }
+           return ans;
     }
 };
