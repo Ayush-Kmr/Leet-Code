4088ca2f54fe9dc66b3fda046de6632cd46a33b6
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-04-10 16:51:44 +0530

    Time: 1701 ms (5.00%), Space: 11.6 MB (44.59%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    int dx[4] = {-1, 0, 1, 0};
-    int dy[4] = {0, -1, 0, 1};
-    
-    bool isSafe(int x, int y, vector<vector<char>>& board) {
-        return (x>=0 and y>=0 and x<board.size() and y<board[0].size());
+    bool isValidPath(int row,int col, int i, int j,int pos, string word){
+        if(pos == word.size()) return true;
+        if(i<row && i>=0 && j>=0 && j<col) return true;
+        return false;
     }
-    
-    bool wordExists(vector<vector<char>>& board, string word, int i, int j, int pos) {
-        if (board[i][j] != word[pos])
-            return false;
+    bool solve(vector<vector<char>>&board, string word, int i , int j, int pos){
+        if (pos == word.size()) return true;
+        if(board[i][j] == ' ') return false;
+        int row = board.size();
+        int col = board[0].size();
+        if(board[i][j] != word[pos]) return false;
         
-        if (pos+1 == word.size())
-            return true;
         
         char temp = board[i][j];
         board[i][j] = ' ';
-        for (int k = 0; k < 4; k++) {
-            int x = dx[k] + i;
-            int y = dy[k] + j;
-            if (isSafe(x, y, board) and wordExists(board, word, x, y, pos+1))
-               return true;
-        }
+        bool ans = false;
+        if(isValidPath(row,col,i+1,j, pos,  word)) ans |= solve(board,word,i+1,j,pos+1);
+        if(isValidPath(row,col,i-1,j, pos,  word)) ans |= solve(board,word,i-1,j,pos+1);
+        if(isValidPath(row,col,i,j+1, pos,  word)) ans |= solve(board,word,i,j+1,pos+1);
+        if(isValidPath(row,col,i,j-1, pos,  word)) ans |= solve(board,word,i,j-1,pos+1);
+        if(temp == word[pos]) ans |= solve(board,word,i,j,pos+1);
         board[i][j] = temp;
-        return false;
-    }    
-    
+        return ans;
+    }
     bool exist(vector<vector<char>>& board, string word) {
-        for(int i = 0; i<board.size(); i++){
-            for(int j = 0; j<board[0].size(); j++){
-                if(wordExists(board, word, i, j, 0))
-                   return true;
+        for(int i=0; i<board.size(); i++){
+            for(int j=0; j<board[0].size(); j++){
+                if(solve(board, word, i ,j, 0)) return true;
             }
         }
         return false;
