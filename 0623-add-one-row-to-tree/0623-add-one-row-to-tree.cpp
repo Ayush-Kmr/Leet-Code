70562cffca719a5e32c86f1f87a6ca3a64f6e060
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-04-16 22:18:07 +0530

    Time: 18 ms (14.77%), Space: 24.5 MB (14.90%) - LeetHub

--- 0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
@@ -12,38 +12,36 @@
 class Solution {
 public:
     TreeNode* addOneRow(TreeNode* root, int val, int depth) {
-        if (!root) return NULL;
-        if (depth == 1) {
+        if(!root) return NULL;
+        if(depth == 1){
             TreeNode* node = new TreeNode(val);
             node->left = root;
             return node;
         }
-
-        queue<TreeNode*> q;
+        queue<TreeNode*>q;
         q.push(root);
-
-        while (!q.empty()) {
+        while(!q.empty()){
             int size = q.size();
-            for (int i = 0; i < size; i++) {
+            while(size--){
                 TreeNode* temp = q.front();
                 q.pop();
-                if (depth == 2) {
+                
+                if(depth == 2){
                     TreeNode* leftNode = new TreeNode(val);
                     leftNode->left = temp->left;
                     temp->left = leftNode;
                     
                     TreeNode* rightNode = new TreeNode(val);
                     rightNode->right = temp->right;
-                    temp->right = rightNode;
+                    temp->right =rightNode;
                 }
                 
-                if (temp->left) q.push(temp->left);
-                if (temp->right) q.push(temp->right);
+                if(temp->left) q.push(temp->left);
+                if(temp->right)q.push(temp->right);
             }
-            
-            if (--depth == 1) break; 
+            depth--;
+            // if(depth == 1) break;
         }
-        
         return root;
     }
 };
