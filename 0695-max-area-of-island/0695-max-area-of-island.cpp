52436b3735096bd508d249dcfad0cf7550d700bd
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-04-18 23:54:38 +0530

    Time: 18 ms (31.12%), Space: 26 MB (99.03%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -4,7 +4,7 @@ public:
         return i >= 0 && i < row && j >=0 && j < col;
     }
 
-    int find(vector<vector<int>>& grid, int i, int j, int count){
+    int find(vector<vector<int>>& grid, int i, int j){
         int row = grid.size();
         int col = grid[0].size();
         int val = grid[i][j];
@@ -18,12 +18,10 @@ public:
        
         grid[i][j] = 0; 
         
-        if(valid(i-1, j, col, row)) ans+=find(grid, i-1, j, count);
-        if(valid(i+1, j, col, row)) ans+=find(grid, i+1, j, count);
-        if(valid(i, j+1, col, row)) ans+=find(grid, i, j+1, count );
-        if(valid(i, j-1, col, row)) ans+=find(grid, i, j-1, count );
-        
-        // grid[i][j] = val; 
+        if(valid(i-1, j, col, row)) ans+=find(grid, i-1, j);
+        if(valid(i+1, j, col, row)) ans+=find(grid, i+1, j);
+        if(valid(i, j+1, col, row)) ans+=find(grid, i, j+1);
+        if(valid(i, j-1, col, row)) ans+=find(grid, i, j-1);
         return ans; 
     }
 
@@ -33,7 +31,7 @@ public:
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[0].size(); j++){
                 if(grid[i][j] == 1){
-                   area=max(area, find(grid, i, j, 1));
+                   area=max(area, find(grid, i, j));
                 }
             }
         }
