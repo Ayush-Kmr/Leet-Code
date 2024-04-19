6af08ec0b0aa4aeb35d3348d75508828a4e16093
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-04-19 13:48:19 +0530

    Time: 19 ms (98.04%), Space: 15.7 MB (81.63%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -4,32 +4,26 @@ public:
     bool check(int i, int j, int  row, int col){
         return (i>=0 && i<row && j>=0 && j<col);
     }
-    int find(vector<vector<char>>& grid,int i, int j,int count){
+    void find(vector<vector<char>>& grid,int i, int j){
         int row = grid.size();
         int col = grid[0].size();
         int val = grid[i][j];
-        
-        if(val == '0'){
-            count +=1;
-            return count;
-        }
+        if(val == '0') return;
         grid[i][j] = '0';
-        if(check(i-1,j,row,col)) find(grid,i-1,j,count);
-        if(check(i+1,j,row,col)) find(grid,i+1,j,count);
-        if(check(i,j+1,row,col)) find(grid,i,j+1,count);
-        if(check(i,j-1,row,col)) find(grid,i,j-1,count);
+        if(check(i-1,j,row,col)) find(grid,i-1,j);
+        if(check(i+1,j,row,col)) find(grid,i+1,j);
+        if(check(i,j+1,row,col)) find(grid,i,j+1);
+        if(check(i,j-1,row,col)) find(grid,i,j-1);
         
-        return count;
         
     }
     int numIslands(vector<vector<char>>& grid) {
-        int count =1;
         int ans =0;
         for(int i=0; i<grid.size(); i++){
             for(int j=0; j<grid[0].size(); j++){
                 if(grid[i][j]=='1'){
-                    find(grid,i,j,count);
-                  ans ++;
+                    find(grid,i,j);
+                     ans ++;
                 }
             }
         }
