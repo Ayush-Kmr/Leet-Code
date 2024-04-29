9a606a2c2524b7b6a4d8f5b86a635e13b115a88a
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-04-30 00:24:56 +0530

    Time: 3 ms (38.04%), Space: 8 MB (73.50%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
+    double solve(double x, long long n){
+        if(n==0) return 1;
+        if(n<=0) return solve(1/x, -1*n);
+        if(n%2 == 0) return solve(x*x,n/2);
+        return x*solve(x*x, (n-1)/2);
+    }
     double myPow(double x, int n) {
-        return pow(x,n);
+        return solve(x,n);
     }
 };
