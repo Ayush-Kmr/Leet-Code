671af5c0a495c8d61cdf7d2dd51638cb1e4e1a00
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-04-29 23:29:47 +0530

    Time: 290 ms (42.12%), Space: 142.7 MB (36.16%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,49 +1,36 @@
 class Solution {
 public:
-    
-    void merge(vector<int>& nums, int start, int mid, int end) {
-        int idx1 = start, idx2 = mid + 1;
-        int i = 0;
-        vector<int> temp(end - start + 1);
-
-        while (idx1 <= mid && idx2 <= end) {
-            if (nums[idx1] < nums[idx2]) {
+    void merge(vector<int>& nums,int start, int mid, int end){
+        int idx1 = start, idx2 = mid+1;
+        int i=0;
+        vector<int>temp(end-start+1);
+        while(idx1 <= mid && idx2 <= end){
+            if(nums[idx1]< nums[idx2]){
                 temp[i++] = nums[idx1++];
-            } else {
+            }
+            else{
                 temp[i++] = nums[idx2++];
             }
         }
-
-        while (idx1 <= mid) {
-            temp[i++] = nums[idx1++];
-        }
-
-        while (idx2 <= end) {
-            temp[i++] = nums[idx2++];
-        }
-
+        while(idx1<= mid) temp[i++] = nums[idx1++];
+        while(idx2<= end) temp[i++] = nums[idx2++];
+        
         for (int k = 0; k < i; k++) {
             nums[start + k] = temp[k];
-        }
+        }    
     }
-
-//     =========================================================================
-    void mergesort(vector<int>&nums, int start, int end){
+    void mergeSort(vector<int>& nums,int start, int end){
         if(start < end){
-            int mid = start + (end-start)/2;
-            mergesort(nums,start,mid);
-            mergesort(nums,mid+1,end);
-            merge(nums,start,mid,end);
+        int mid = start + (end - start)/2;
+        mergeSort(nums,start,mid);
+        mergeSort(nums,mid+1,end);
+        merge(nums,start,mid,end);
         }
     }
-    
-//     ===========================================================================
     vector<int> sortArray(vector<int>& nums) {
-        int n = nums.size();
-        int start=0;
-        int end = n-1;
-        mergesort(nums,start,end);
-        
+        int start =0;
+        int end = nums.size() -1;
+        mergeSort(nums,start,end);
         return nums;
     }
 };
