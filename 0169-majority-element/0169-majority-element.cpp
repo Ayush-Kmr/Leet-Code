5f03c1c83815f5cbb954fd53c373c93fe1b3f399
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-04-30 02:49:16 +0530

    Time: 20 ms (39.37%), Space: 26.6 MB (71.13%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -6,8 +6,9 @@ public:
         for(int i=0; i<nums.size(); i++){
             if(count == 0){
                 candidate =  nums[i];
+                count =1;
             }
-            if(candidate == nums[i]){
+            else if(candidate == nums[i]){
                 count++;
             }
             else count--;
