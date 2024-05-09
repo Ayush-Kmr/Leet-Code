306e15e4f65043e046abf687694235c20951539f
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-05-09 23:59:22 +0530

    Time: 11 ms (47.59%), Space: 14.7 MB (47.23%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    using int2=pair<int, int>;
     vector<string> findRelativeRanks(vector<int>& score) {
-        int n=score.size();
-        vector<int2> sIdx(n);
-        for(int i=0; i<n; i++)
-            sIdx[i]={score[i], i};
-        sort(sIdx.rbegin(), sIdx.rend());
-        vector<string> ans(n);
-        ans[sIdx[0].second]="Gold Medal";
-        if( n>1) ans[sIdx[1].second]="Silver Medal";
-        if (n>2) ans[sIdx[2].second]="Bronze Medal";
-        for(int i=3; i<n; i++)
-            ans[sIdx[i].second]=to_string(i+1);
+        vector<int> temp(score.size());
+        vector<string> ans(score.size());
+        for (int i = 0; i < score.size(); i++) {
+            temp[i] = score[i];
+        }
+        sort(temp.begin(), temp.end(), greater<int>());
+        map<int, int> mp;
+        for (int i = 0; i < temp.size(); i++) {
+            mp[temp[i]] = i + 1;
+        }
+        
+        for (int i = 0; i < score.size(); i++) {
+            if (mp[score[i]] == 1) {
+                ans[i] = "Gold Medal";
+            } else if (mp[score[i]] == 2) {
+                ans[i] = "Silver Medal";
+            } else if (mp[score[i]] == 3) {
+                ans[i] = "Bronze Medal";
+            } else {
+                ans[i] = to_string(mp[score[i]]);
+            }
+        }
+        
         return ans;
     }
 };
