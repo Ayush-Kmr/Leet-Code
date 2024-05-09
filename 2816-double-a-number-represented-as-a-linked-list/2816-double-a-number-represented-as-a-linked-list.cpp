25e3a0b831cd8cd2d17a637bf5babaf6e6a19559
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-05-10 03:13:51 +0530

    Time: 160 ms (84.84%), Space: 120.6 MB (82.81%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
@@ -1,16 +1,41 @@
 class Solution {
 public:
-    bool carry(ListNode* head){
-        if (!head) return 0;
-        bool c=carry(head->next);
-        int x=head->val*2+c;
-        bool c0=0;
-        if (x>=10) x-=10, c0=1;
-        head->val=x;
-        return c0;
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* forward = NULL;
+        while (curr) {
+            forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        return prev;
     }
+
     ListNode* doubleIt(ListNode* head) {
-        if (carry(head)) head=new ListNode(1, head);
-        return head;
+        ListNode* curr = reverseList(head);
+        ListNode* newHead = NULL; 
+        ListNode* prev = NULL; 
+        int carry = 0;
+
+        while (curr) {
+            int newVal = curr->val * 2 + carry;
+            curr->val = newVal % 10;
+            carry = newVal / 10;
+
+            if (prev == NULL) {
+                newHead = curr;
+            }
+            prev = curr;
+            curr = curr->next;
+        }
+
+        if (carry != 0) {
+            ListNode* newNode = new ListNode(carry);
+            prev->next = newNode;
+        }
+
+        return reverseList(newHead);
     }
 };
