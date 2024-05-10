d17da8e7fe7182e215f2d69d3680744796df0e7a
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-05-10 22:17:56 +0530

    Time: 1135 ms (15.87%), Space: 257.5 MB (10.83%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        double low = 0.0, high = 1.0;
-        int p = 0, q = 1, count = 0;
-        while (low < high) {
-            double mid = (low + high) / 2;
-            count = 0;
-            p = 0;
-            for (int i = 0, j = 1; i < arr.size() - 1; ++i) {
-                while (j < arr.size() && arr[i] >= arr[j] * mid) {
-                    ++j;
-                }
-                count += arr.size() - j;
-                if (j < arr.size() && p * arr[j] < q * arr[i]) {
-                    p = arr[i];
-                    q = arr[j];
-                }
-            }
-            if (count == k) {
-                break;
-            } else if (count < k) {
-                low = mid;
-            } else {
-                high = mid;
+        int n = arr.size();
+        priority_queue<vector<double>>pq;
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                double div = (double)arr[i]/arr[j];
+                pq.push(vector<double>{div,(double)arr[i], (double)arr[j]});
+                
+                if(pq.size()>k) pq.pop();
             }
         }
-        return {p, q};
+        auto vec = pq.top();
+        vector<int>result(2);
+        result[0] = vec[1];
+        result[1] = vec[2];
+        
+        return result;
     }
 };
