d38e98b552b435f448645af4dfef5264d923045c
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-06-27 01:19:41 +0530

    Time: 2 ms (96.34%), Space: 45.6 MB (75.92%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-    private void inOrder(TreeNode root, List<Integer>vec){
+    private void inOrder(TreeNode root,List<Integer>vec){
         if(root == null) return ;
         inOrder(root.left,vec);
         vec.add(root.val);
@@ -23,16 +23,18 @@ class Solution {
     
     private TreeNode construct(int start, int end, List<Integer>vec){
         if(start > end) return null;
-        
-        int mid = start + (end - start) /2;
+        int mid = start + (end - start)/2;
         TreeNode root = new TreeNode(vec.get(mid));
         root.left = construct(start,mid-1,vec);
-        root.right =construct(mid+1,end,vec);
+        root.right = construct(mid+1,end,vec);
         return root;
-    }
+    }    
+    
     public TreeNode balanceBST(TreeNode root) {
+        if(root == null) return null;
         List<Integer> vec = new ArrayList<>();
         inOrder(root,vec);
         return construct(0,vec.size()-1,vec);
+        
     }
 }
