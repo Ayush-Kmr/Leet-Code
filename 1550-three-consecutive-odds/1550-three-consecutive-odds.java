5dbc2e4f7ac2d101cee5e97ae00fee79d618d831
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-07-02 00:28:13 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (93.34%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int n = arr.length;
+        for(int i=0; i<=n-3; i++){
+            if(arr[i]%2!=0 && arr[i+1]%2!=0 && arr[i+2]%2!=0) return true;
+        }
+        return false;
+    }
+}
