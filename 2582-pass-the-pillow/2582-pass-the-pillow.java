6e73b8693dff514a49d4beac9ef4a87cbc9b6b30
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-07-07 02:01:14 +0530

    Time: 1 ms (28.42%), Space: 40.6 MB (16.67%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+        int dir =1;
+        int idx =1;
+        while(time>0){
+            if(idx + dir >=1 && idx +dir <=n){
+                idx+=dir;
+                time--;
+            }
+        else dir*=-1;
+        }
+        return idx;
+    }
+}
