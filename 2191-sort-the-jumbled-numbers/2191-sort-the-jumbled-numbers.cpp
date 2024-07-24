f85a693376754d1e4a22a27212443dc0d0690c91
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-07-25 01:13:43 +0530

    Time: 120 ms (98.80%), Space: 85 MB (86.85%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    int getNumber(int num, vector<int> &mapping) {
-        if (num == 0) return mapping[0];
-
-        int opt = 0;
-        int factor = 1;
-
-        while (num) {
-            opt += mapping[num % 10] * factor;
-            factor *= 10;
-            num /= 10;
-        }
-
-        return opt;
-    }
-
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(), nums.end(), [&](int a, int b) {
-            int mapped_a = getNumber(a, mapping);
-            int mapped_b = getNumber(b, mapping);
-            if (mapped_a == mapped_b)
-                return false;
-            return mapped_a < mapped_b;
-        });
-        return nums;
+        vector<pair<int, int>> mp;
+
+        for(int i = 0; i<n;i++){
+
+            int temp = nums[i];
+            int newNum = 0;
+            int tens = 1;
+
+            if(temp==0)
+                newNum = mapping[0];
+
+            while(temp>0){
+                newNum += tens*mapping[temp%10];
+                tens*=10;
+                temp /= 10;
+            }
+
+            mp.push_back({newNum, i});
+        }
+
+        sort(mp.begin(), mp.end());
+
+        vector<int> ans(n);
+        for(int i=0;i<n;i++)
+            ans[i] = nums[mp[i].second];
+        
+        return ans;
     }
 };
