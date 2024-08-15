545fcaa4ce9d855fcdea341acaee8a6395d5b4bd
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-08-15 16:15:21 +0530

    Time: 72 ms (57.53%), Space: 86 MB (32.71%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int countF =0;
-        int countT =0;
-        for(int i=0; i<bills.size(); i++){
-            if(bills[i] == 5)countF++;
-            else if(bills[i] == 10){
-                countT++;
-                if(countF) countF--;
-                else return false;
-            }
-            else if(bills[i] == 20){
-                if(countT && countF){
-                    countT--;
+        int countF = 0, countT = 0;
+
+        for(int bill : bills) {
+            switch(bill) {
+                case 5:
+                    countF++;
+                    break;
+                case 10:
+                    if (countF == 0) return false;
                     countF--;
-                }
-                else if(countT && !countF) return false;     
-                else if(countF >=3) countF -= 3;
-                else if(!countT || !countF) return false;
+                    countT++;
+                    break;
+                case 20:
+                    if (countT > 0 && countF > 0) {
+                        countT--;
+                        countF--;
+                    } else if (countF >= 3) {
+                        countF -= 3;
+                    } else {
+                        return false;
+                    }
+                    break;
             }
         }
+
         return true;
     }
 };
