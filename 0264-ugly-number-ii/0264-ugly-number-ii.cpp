5857a6ce647b7c0dc75c31ccb13b26119314d879
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-08-18 18:04:04 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (56.48%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int>arr(n+1);
+        int i2,i3,i5;
+        
+//         1st UGLY Number
+        i2 = i3 = i5 = 1;
+        
+        arr[1] = 1;
+        for(int i=2; i<=n; i++){
+            int i2UglyNum = arr[i2]*2;
+            int i3UglyNum = arr[i3]*3;
+            int i5UglyNum = arr[i5]*5;
+            
+            int minUglyNum = min({i2UglyNum,i3UglyNum,i5UglyNum});
+            arr[i]= minUglyNum;
+            
+            // jisk minimum hua uske pionter ko badhate the
+            if(minUglyNum == i2UglyNum) i2++;
+            if(minUglyNum == i3UglyNum) i3++;
+            if(minUglyNum == i5UglyNum) i5++;
+            
+            
+        }
+        
+        return arr[n];
+    }
+};
