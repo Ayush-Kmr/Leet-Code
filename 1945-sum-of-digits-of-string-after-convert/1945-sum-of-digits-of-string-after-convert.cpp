ac83ceba79f993e249d9e379652626261453b6fc
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-09-04 03:40:35 +0530

    Time: 0 ms (100.00%), Space: 10.6 MB (6.49%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int getLucky(std::string s, int k) {
-        std::unordered_map<char, int> letterToValue;
+        unordered_map<char, int> letterToValue;
+        string value;
         for (char i = 'a'; i <= 'z'; ++i) {
             letterToValue[i] = i - 'a' + 1; 
         }
-        string value;
-        for (char c : s) {
-            if (std::islower(c)) {
-                value += to_string(letterToValue[c]);
-            }
-        }
-
-        if (value.empty()) {
-            return 0;
-        }
+        for (char c : s) value += to_string(letterToValue[c]);
+        if (value.empty()) return 0;
         int temp = 0;
-        for (char c : value) {
-            temp += c - '0';
-        }
+        for (char c : value) temp += c - '0';
         for (int i = 1; i < k; ++i) {
             int sum = 0;
-            while (temp > 0) {
+            while (temp) {
                 sum += temp % 10;
                 temp /= 10;
             }
