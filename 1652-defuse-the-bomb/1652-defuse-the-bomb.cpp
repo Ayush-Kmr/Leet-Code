5b3ff68d992a0993cd1f826148c1dd671d8b9f4e
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2024-11-19 01:16:47 +0530

    Time: 0 ms (100.00%), Space: 10.4 MB (60.16%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
-        vector<int> ans;
         int n = code.size();
-        for (int i = 0; i < code.size(); i++) {
-            long long sum = 0;
+        vector<int> ans(n, 0);
 
-            if (k > 0) {
-                for (int j = i + 1, count = 0; count < k; j++, count++) {
-                    sum += code[j % n]; 
-                }
-                ans.push_back(sum);
-            } else if (k == 0) {
-                std::fill(code.begin(), code.end(), 0);
-                return code;
-            } else {
-                for (int j = i + k, count = 0; count < -k; j++, count++) {
-                    sum += code[(j + n) % n]; 
-                }
-                ans.push_back(sum);
-            }
+        if (k == 0) {
+            return ans; 
+        }
+
+        int i, j;
+        if (k > 0) {
+            i = 1;
+            j = k;
+        } else {
+            i = n + k; 
+            j = n - 1; 
+        }
+
+        
+        int windowSum = 0;
+        for (int pointer = i; pointer <= j; pointer++) {
+            windowSum += code[pointer % n];
+        }
+
+        
+        for (int index = 0; index < n; index++) {
+            ans[index] = windowSum; 
+            windowSum -= code[i % n]; 
+            i++;
+            j++;
+            windowSum += code[j % n]; 
         }
 
         return ans;
