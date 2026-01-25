bbde7edd135df05a7837a592b7a562106ef769c0
Ayush Kumar <102462699+Ayush-Kmr@users.noreply.github.com>
2026-01-25 14:44:10 +0530

    Time: 33 ms (51.99%) | Memory: 19.1 MB (53.20%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int solve(vector<int>& coins, int amount, vector<int>&dp){
+        if(amount == 0) return 0;
+        if(amount < 0) return 1e9;
+        if(dp[amount]!= -1) return dp[amount];
+        int ans = 1e9;
+        for(int i=0; i<coins.size(); i++)ans = min(ans, 1+solve(coins, amount - coins[i], dp));
+        return dp[amount] = ans;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int>dp(amount+1, -1);
+        int ans = solve(coins, amount, dp);
+        return (ans >= 1e9) ? -1 : ans;
+    }
+};
